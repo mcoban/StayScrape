b9a347af69326adc1cc5476fb7b617a4721d6c6e
müslüm çoban <mcoban86@gmail.com>
2017-04-06 21:03:52 +0300

    lazyload

---
+++ stayscrape/static/js/custom.js
@@ -0,0 +1,34 @@
+window.onload = function() {
+
+
+	// $('.lazyload').each(function() {
+	// 	var img = $(this);
+	// 	var src = img.attr('data-origin');
+	// 	$(this).attr('src', src);
+	// });
+
+	// lazyload = function() {
+	// 	var lazy_images = $('.lazyload');
+	// 	$.each(lazy_images, function() {
+	// 		var img = $(this);
+	// 		var src = img.attr('data-origin');
+	// 		$(this).attr('src', src);
+	// 	});
+	// }
+
+	// lazyload();
+
+};
+
+
+$(window).bind("load", function() {
+
+	setTimeout(function() {
+		$('.lazyload').each(function() {
+			var img = $(this);
+			var src = img.attr('data-origin');
+			$(this).attr('src', src);
+		});
+	}, 3000);
+
+});
