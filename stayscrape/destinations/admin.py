1dc840fa270669934dfdddf6031c587212968f6f
müslüm çoban <mcoban86@gmail.com>
2017-04-07 00:13:48 +0300

    meta_title

--- stayscrape/destinations/admin.py
+++ stayscrape/destinations/admin.py
@@ -3,5 +3,5 @@ from .models import Place
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
-	list_display = ('name', 'page_title')
+	list_display = ('name', 'page_title', 'meta_title')
 	search_fields = ('name', )
