205560d13182246040dd4cccf245090a05694074
müslüm çoban <mcoban86@gmail.com>
2017-06-30 01:53:53 +0300

    pprint

--- stayscrape/stayscrape/views.py
+++ stayscrape/stayscrape/views.py
@@ -15,7 +15,9 @@ def messenger_callback(request):
 	if request.body:
 		incomming_message = json.loads(request.body.decode('utf-8'))
 		for entry in incomming_message['entry']:
-			pprint.pprint(entry)
+			for message in entry['messaging']:
+				if 'message' in message:
+					pprint.pprint(message)
 
 	
 	return HttpResponse('ok')
