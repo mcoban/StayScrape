baa3491afec54b8dec4f2b64d383247ed99d967d
müslüm çoban <mcoban86@gmail.com>
2017-06-30 01:43:29 +0300

    pprint

--- stayscrape/stayscrape/views.py
+++ stayscrape/stayscrape/views.py
@@ -10,6 +10,6 @@ def messenger_callback(request):
 	if request.GET.get('hub.verify_token') == 'mcoban':
 		return HttpResponse(request.GET.get('hub.challenge'))
 
-	pprint.pprint(request)
+	pprint.pprint(request.POST)
 	
 	return HttpResponse('ok')
