927ecab1fb3f644ffd6680b01d38e18479678c37
müslüm çoban <mcoban86@gmail.com>
2017-06-30 12:14:54 +0300

    import requests

--- stayscrape/fb_bot/views.py
+++ stayscrape/fb_bot/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
 import json, pprint
+import requests
 
 
 # class MessengerBotBiew(generic.View):
